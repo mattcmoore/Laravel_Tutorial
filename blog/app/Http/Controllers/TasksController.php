6663c5a63faaa6e8c99c7ac131b9533b14d0a6f7
Matt C. Moore <matt@mattcmoore.com>
2018-01-24 05:49:50 -0800

    updated show() controller method so that it uses route method binding to handle wildcard from path-to-view (From Laravel 5.4 Tutorial Episode 9

--- blog/app/Http/Controllers/TasksController.php
+++ blog/app/Http/Controllers/TasksController.php
@@ -13,9 +13,8 @@ class TasksController extends Controller
     	
     	return view('tasks.index',compact('tasks'));
   	}
-  	// $id expected from {task} wildcard in view path i.e. function show($id) 
-  	public function show($id){
-  		$task = Task::find($id);
+  	public function show(Task $task){
+  	//	$task = Task::find($id);
     	
     	return view('tasks.show',compact('task'));
   	}
