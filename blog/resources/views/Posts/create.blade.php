cff60b7aa65f46a47fbbff2e66559ad41b432bb1
Matt C. Moore <matt@mattcmoore.com>
2018-01-25 06:01:13 -0800

    built create post Bootstrap form w/ action=/posts on create.blade.php, routed '/posts/create' to create.blade.php, secured create post form with crsf_field(), routed  requestsvia store() method (store() calls Post::create() to request data from 'title' and'body' form fields then save  data to correspoding columns in posts table, thenredirects to the homepage), restricted Post::create() input by setting  in Post)(From Laravel 5.4 Tutorial Epidode 11)

--- blog/resources/views/Posts/create.blade.php
+++ blog/resources/views/Posts/create.blade.php
@@ -7,7 +7,7 @@
 {{csrf_field()}}
   <div class="form-group">
     <label for="title">Title:</label>
-    <input type="text" class="form-control" id="title" name="title">
+    <input type="text"  id="title" name="title" class="form-control">
   </div>
 
   <div class="form-group">
